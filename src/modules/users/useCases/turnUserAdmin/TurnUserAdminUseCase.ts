deffdc40b3a5edb41c1cba57c04ba0ddc8a31f24
Alfiado Constantino <alfiado02@gmail.com>
2021-04-28 22:09:07 +0200

    finalizing the chalenge

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -9,7 +9,13 @@ class TurnUserAdminUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const existsUser = this.usersRepository.findById(user_id);
+
+    if (!existsUser) throw new Error("Mensagem do erro");
+
+    const userAdmin = this.usersRepository.turnAdmin(existsUser);
+
+    return userAdmin;
   }
 }
 
